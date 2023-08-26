9dccae2714b81e3d3eb0c618b0e25b0e0d50fd15
basselamr <basselamr1@gmail.com>
2023-08-26 17:13:41 +0300

    add favorites feature

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.scss']
+  styleUrls: ['./favorites.component.scss'],
 })
 export class FavoritesComponent {
-
+  @Output() close$ = new EventEmitter<boolean>();
+  currency!: string;
+  rate!: number;
+  close() {
+    // this.close$.emit(true);
+    setTimeout(() => {
+      this.close$.emit(true);
+    }, 1000);
+  }
 }

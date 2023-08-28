f9b24988f1ce63a76043fc15eff94992980b5b66
Youssef Gharib <yousifgharib44@gmail.com>
2023-08-28 17:05:07 +0300

    update branch

--- src/app/currency-converter/compare/compare.component.ts
+++ src/app/currency-converter/compare/compare.component.ts
@@ -9,27 +9,40 @@ import { AppService } from 'src/app/services/app.service';
   styleUrls: ['./compare.component.scss']
 })
 export class CompareComponent {
+  loading = false;
   selectedCurrency: ICurrency = {
     "id": 11,
     "currencyCode": "EGP",
     "flagUrl": "https://flagcdn.com/h60/eg.png"
-};
-
+  };
+  selectedCurrency1: ICurrency =  {
+    "id": 1,
+    "currencyCode": "USD",
+    "flagUrl": "https://flagcdn.com/h60/us.png"
+  };
+  selectedCurrency2: ICurrency = {
+    "id": 3,
+    "currencyCode": "GBP",
+    "flagUrl": "https://flagcdn.com/h60/gb.png"
+  };
+  amount: number= 1;
   currencyFrom!: ICurrency;
   currencyTo!: ICurrency;
-  result!: number;
+  result: number=1;
   constructor(private appService: AppService) { }
-
+  
   submit() {
+    this.loading = true;
     let data: ICurrencyConvert = {
       from: this.currencyFrom.currencyCode,
       to: this.currencyTo.currencyCode,
-      amount: 1
+      amount: this.amount,
     }
 
     this.appService.convert(data).subscribe({
       next: (res) => {
         this.result = res.result;
+        this.loading = false;
       }
     })
   }
